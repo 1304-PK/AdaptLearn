13cc06fdcbc8bf392e106c4192cdfe2c8985c4f1
PK-1304 <pushkark1304@gmail.com>
2026-03-26 22:19:40 +0530

    implemented auth provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LandingPage from './pages/LandingPage'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LandingPage />
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
   </StrictMode>,
 )

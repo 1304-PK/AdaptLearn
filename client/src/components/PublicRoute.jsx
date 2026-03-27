6d2540d14f2e0812f1f38b1c8512d4a2f4f3da9f
PK-1304 <pushkark1304@gmail.com>
2026-03-27 21:59:41 +0530

    minor changes

--- client/src/components/PublicRoute.jsx
+++ client/src/components/PublicRoute.jsx
@@ -1,18 +1,43 @@
 import { useAuth } from "../context/AuthContext"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import supabase from "../lib/supabaseClient"
 
 const PublicRoute = ({children}) => {
 
-    const {session, loading} = useAuth()
+    const {session, loading, user} = useAuth()
     const navigate = useNavigate()
 
     useEffect(() => {
+        const getRole = async () => {
+    if (!user) return;
+
+          let role;
+
+    const { data, error } = await supabase
+      .from('hr_table')
+      .select('role')
+      .eq('id', user.id)
+      .single();
+
+    if (data) role = "hr"
+    else{
+      const { data: eData, error: eError } = await supabase
+      .from('employees')
+      .select('role')
+      .eq('id', user.id)
+      .single();
+      role = "employees"
+    }
+
+    if (role === 'hr') {
+      navigate("/admin/dashboard", {replace: true})
+    } else if (data?.role === 'employee') {
+      console.log("employee"); 
+    }
+  };
         if (!loading && session){
-            console.log("session exists")
-        }
-        else{
-            console.log("session doesn't exist")
+            getRole()
         }
     }, [session, loading, navigate])
 

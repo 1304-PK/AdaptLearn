2513b1f710d14599c96a49a4deeb680b94f44539
PK-1304 <pushkark1304@gmail.com>
2026-03-29 16:42:55 +0530

    employee dashboard page

--- client/src/pages/EmployeeDashboard.jsx
+++ client/src/pages/EmployeeDashboard.jsx
@@ -2,13 +2,26 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar.jsx"
 import { TooltipProvider } from "@/components/ui/tooltip" 
 import { AppSidebar } from "@/components/AppSidebar"
 import { Outlet } from "react-router-dom"
+import supabase from "../lib/supabaseClient"
+import { useAuth } from "../context/AuthContext"
 
 export default function EmployeeDashboard() {
+
+  const handleLogOut = async() => {
+    try{
+      const {error} = await supabase.auth.signOut()
+
+      if (error) throw error
+    } catch(error){
+      console.error(error.message)
+    }
+  }
+
   return (
     <TooltipProvider>
         <SidebarProvider>
           {/* 1. Sidebar */}
-          <AppSidebar />
+          <AppSidebar handleLogOut={handleLogOut}/>
 
           {/* Main Content */}
           <main className="flex-1">

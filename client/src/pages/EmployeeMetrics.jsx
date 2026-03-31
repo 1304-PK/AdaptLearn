50fd0340363ba0004133000584ffbf674af6006d
PK-1304 <pushkark1304@gmail.com>
2026-03-31 12:39:20 +0530

    metrics page for employee

--- client/src/pages/EmployeeMetrics.jsx
+++ client/src/pages/EmployeeMetrics.jsx
@@ -4,19 +4,23 @@ import MetricCircle from "../components/MetricCircle"
 import { useEffect, useState } from "react"
 import supabase from "../lib/supabaseClient"
 import { useAuth } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
 
 const EmployeeMetrics = () => {
 
+    const navigate = useNavigate()
+
     const [metrics, setMetrics] = useState(null)
-    const [skillBars, setSkillBars] = useState(null)
     const session = useAuth()
 
     useEffect(() => {
         const getMetricData = async () => {
             const { data, error } = await supabase
                 .from("employee_metrics")
-                .select("resume_metrics")
+                .select("resume_metrics, created_at")
                 .eq("user_id", session.user.id)
+                .order("created_at", {ascending: false})
+                .limit(1)
                 .single()
 
             if (error) throw error
@@ -161,7 +165,25 @@ const EmployeeMetrics = () => {
 
                 </div>
             </>
-        );
+        )
+
+        return(
+            <div className="flex items-center justify-center min-h-screen bg-[#0d0d0d] p-6">
+  <div className="flex flex-col items-center justify-center max-w-sm w-full p-12 border border-slate-700 border-dashed rounded-3xl bg-[#1a1a1a] backdrop-blur-sm text-center">
+    <div className="mb-8">
+      
+      <h3 className="text-lg font-medium text-slate-100">No Metrics present</h3>
+      <p className="mt-2 text-sm text-slate-400 leading-relaxed">
+        Upload your resume to get one
+      </p>
+    </div>
+
+    <button onClick={() => {navigate("/employee/upload-documents")}} className="w-full py-3 px-6 bg-slate-100 hover:bg-white text-slate-950 text-sm font-semibold rounded-xl transition-all duration-200 ease-in-out transform active:scale-[0.98] cursor-pointer">
+      Upload Resume
+    </button>
+  </div>
+</div>
+        )
 }
 
 export default EmployeeMetrics

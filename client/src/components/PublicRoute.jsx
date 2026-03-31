0f833019683726fe18c11da9a84b6ea1a0f4ff12
PK-1304 <pushkark1304@gmail.com>
2026-03-31 12:02:50 +0530

    updated route to now show blank page

--- client/src/components/PublicRoute.jsx
+++ client/src/components/PublicRoute.jsx
@@ -1,7 +1,6 @@
 import { useAuth } from "../context/AuthContext"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
-import supabase from "../lib/supabaseClient"
 
 const PublicRoute = ({children}) => {
 
